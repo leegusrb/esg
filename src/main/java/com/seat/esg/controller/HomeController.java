9ee11687e33cec34c91f359e5b732c736e66d4a5
Lee Hyeon Gyu <jeffsep24@gmail.com>
2022-11-19 18:00:05 +0900

    seat info

--- src/main/java/com/seat/esg/controller/HomeController.java
+++ src/main/java/com/seat/esg/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.seat.esg.controller;
 
 import com.seat.esg.domain.Seat;
+import com.seat.esg.domain.SeatStatus;
 import com.seat.esg.repository.SeatRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -17,8 +18,24 @@ public class HomeController {
 
     @GetMapping("/")
     public String home(Model model) {
-        List<Seat> seats = seatRepository.findAll();
-        model.addAttribute("seats", seats);
+        make(model);
         return "home";
+    }
+
+    @GetMapping("/manager")
+    public String manager(Model model) {
+        make(model);
+        return "manager";
+    }
+
+    private void make(Model model) {
+        List<Seat> seats = seatRepository.findAll();
+        int awaySize = seatRepository.findByStatus(SeatStatus.AWAY).size();
+        int emptySize = seatRepository.findByStatus(SeatStatus.EMPTY).size();
+        int fullSize = seatRepository.findByStatus(SeatStatus.FULL).size();
+        model.addAttribute("seats", seats);
+        model.addAttribute("awaySize", awaySize);
+        model.addAttribute("emptySize", emptySize);
+        model.addAttribute("fullSize", fullSize);
     }
 }
