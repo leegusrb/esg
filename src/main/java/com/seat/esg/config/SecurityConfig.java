3ecdfd0006e5b2792afb5d620b94683d30007782
Lee Hyeon Gyu <jeffsep24@gmail.com>
2022-12-01 18:52:17 +0900

    manager login

--- src/main/java/com/seat/esg/config/SecurityConfig.java
+++ src/main/java/com/seat/esg/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
-                        .antMatchers("/**", "/account/register", "/css/**", "/js/**").permitAll()
+                        .antMatchers("/", "/home", "/css/**", "/js/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin((form) -> form
