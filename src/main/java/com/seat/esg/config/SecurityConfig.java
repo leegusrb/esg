e94bc32c540cf097da03086d99a2c7a09d664300
Lee Hyeon Gyu <jeffsep24@gmail.com>
2022-12-02 17:18:35 +0900

    update security config

--- src/main/java/com/seat/esg/config/SecurityConfig.java
+++ src/main/java/com/seat/esg/config/SecurityConfig.java
@@ -32,8 +32,8 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
-                        .antMatchers("/", "/home", "/css/**", "/js/**").permitAll()
-                        .anyRequest().authenticated()
+                        .antMatchers("/manager").authenticated()
+                        .anyRequest().permitAll()
                 )
                 .formLogin((form) -> form
                         .loginPage("/account/login")
